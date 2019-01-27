4464b6f657daf48010059b1536e3d99d520ecfb9
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-27 03:40:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   new_array = []
   while i < collection.length
-    new_array << yield(array[i])
+    new_array << yield(collection[i])
     i = i + 1
   end
   new_array
